8636b56a31cbbeb4d4476a60a6d4ec9277a87d1f
marekj <macki@int.pl>
2022-05-18 00:30:20 +0200

    add AboutApp component

---
+++ src/components/AboutApp.vue
@@ -0,0 +1,38 @@
+<template>
+<q-dialog ref="dialog"  @hide="onDialogHide">
+    <q-card class="q-dialog-plugin">
+
+        <q-card-actions align="right">
+            <q-btn color="primary" label="OK" @click="onOKClick" />
+        </q-card-actions>
+    </q-card>
+</q-dialog>
+</template>
+
+<script>
+export default {
+    name: 'AboutApp',
+
+    emits: [
+        'hide'
+    ],
+
+    methods: {
+        show: function () {
+            this.$refs.dialog.show();
+        },
+
+        hide() {
+            this.$refs.dialog.hide()
+        },
+
+        onDialogHide() {
+            this.$emit('hide')
+        },
+
+        onOKClick() {
+            this.hide()
+        },
+    }
+}
+</script>

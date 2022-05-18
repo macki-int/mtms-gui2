83a32438abb306ddc73562864f8de4c7e5c027fb
marekj <macki@int.pl>
2022-05-19 00:33:37 +0200

    add logo and title in AboutApp

--- src/components/AboutApp.vue
+++ src/components/AboutApp.vue
@@ -1,6 +1,16 @@
 <template>
-<q-dialog ref="dialog"  @hide="onDialogHide">
+<q-dialog ref="dialog" @hide="onDialogHide">
     <q-card class="q-dialog-plugin">
+        <q-card-section class="bg-primary">
+            <div class="row">
+                <div class="column">
+                    <img alt="MTms logo" src="~assets/logo.png" style="width: 120px; height: 120px">
+                </div>
+                <div class="column">
+                    <h2 class="text-h2 text-white q-my-lg q-ml-lg"><strong>MTMS</strong></h2>
+                </div>
+            </div>
+        </q-card-section>
 
         <q-card-actions align="right">
             <q-btn color="primary" label="OK" @click="onOKClick" />

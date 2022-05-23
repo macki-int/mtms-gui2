634aeb742399d160a623ef4d6446da0427b711b5
marekj <macki@int.pl>
2022-05-24 00:31:57 +0200

    add StatisticInfo page

---
+++ src/pages/StatisticInfo.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="q-pa-md" style="max-width: 150vh">
+
+    <q-card>
+        <!-- <q-card-section class="text-h6">
+      </q-card-section>-->
+        <q-card-section>
+            <img alt="Lokalizacja" src="~assets/View1.jpg" style="width: 143vh">
+        </q-card-section>
+
+    </q-card>
+    <!-- <q-btn flat color=“brown” size=“lg” align=“right” label=“WATCHING” @click="getReadouts" /> -->
+</div>
+</template>
+
+<script>
+export default {
+    name: 'StatisticInfo',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>

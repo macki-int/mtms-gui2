938ccf30d47c9a9c77e5f5772a08ca120dbc4b9e
marekj <macki@int.pl>
2022-06-09 00:05:30 +0200

    change statistic background

--- src/pages/StatisticInfo.vue
+++ src/pages/StatisticInfo.vue
@@ -1,77 +1,70 @@
 <template>
-<q-page class="q-pa-lg">
-    <div class="row q-pa-md" style="max-width:1220px">
-        <div class="col">
-            <q-card class="q-pa-md" flat bordered>
-                <div class="text-h6 text-center">Autonomiczny system pomiaru zmienności poziomu wód gruntowych</div>
-                <div class="text-subtitle3">System zainstalowano na potrzeby oceny wielkości wahań zwierciadła
-                    wód gruntowych w rejonie północnej ściany hali Saint Gobain Innovative Materials Polska Sp. z
-                    o.o.,
-                    Sekurit Polska przy ul.Szklanych domów w Dąbrowie Górniczej na działce o nr ewid. 4179/5.</div>
-            </q-card>
+    <q-page class="q-pa-lg">
+        <div class="row q-pa-md" style="max-width:1220px">
+            <div class="col">
+                <q-card class="q-pa-md" flat bordered>
+                    <div class="text-h6 text-center">Autonomiczny system pomiaru zmienności poziomu wód gruntowych</div>
+                    <div class="text-subtitle3">System zainstalowano na potrzeby oceny wielkości wahań zwierciadła
+                        wód gruntowych w rejonie północnej ściany hali Saint Gobain Innovative Materials Polska Sp. z
+                        o.o.,
+                        Sekurit Polska przy ul.Szklanych domów w Dąbrowie Górniczej na działce o nr ewid. 4179/5.</div>
+                </q-card>
+            </div>
         </div>
-    </div>
-    <div class="row" style="max-width:1220px">
-        <div class="col-7 q-pa-md">
-            <q-card class="q-pa-md" style="height: 480px" flat bordered>
+        <div class="row" style="max-width:1220px">
+            <div class="col-7 q-pa-md">
+                <q-card class="q-pa-md" style="height: 480px" flat bordered>
 
-                <div class="q-pa-lg">
-                    <q-carousel swipeable animated v-model="slide" :autoplay="autoplay" thumbnails infinite>
+                    <div class="q-pa-lg">
+                        <q-carousel swipeable animated v-model="slide" :autoplay="autoplay" thumbnails infinite>
 
-                        <q-carousel-slide :name="1" img-src="~assets/View1.jpg" />
-                        <q-carousel-slide :name="2" img-src="~assets/View2.jpg" />
-                        <q-carousel-slide :name="3" img-src="~assets/View3.jpg" />
-                        <q-carousel-slide :name="4" img-src="~assets/View4.jpg" />
-                        <q-carousel-slide :name="5" img-src="~assets/View5.jpg" />
-                        <q-carousel-slide :name="6" img-src="~assets/View6.jpg" />
-                    </q-carousel>
-                </div>
-            </q-card>
+                            <q-carousel-slide :name="1" img-src="~assets/View1.jpg" />
+                            <q-carousel-slide :name="2" img-src="~assets/View2.jpg" />
+                            <q-carousel-slide :name="3" img-src="~assets/View3.jpg" />
+                            <q-carousel-slide :name="4" img-src="~assets/View4.jpg" />
+                            <q-carousel-slide :name="5" img-src="~assets/View5.jpg" />
+                            <q-carousel-slide :name="6" img-src="~assets/View6.jpg" />
+                        </q-carousel>
+                    </div>
+                </q-card>
+            </div>
+            <div class="col-5 q-pa-md">
+                <q-card class="q-pa-lg" style="height: 480px" flat bordered>
+                    <div class="q-pa-md q-gutter-sm">
+                        <q-banner dense rounded class="bg-primary text-white">
+                            <template v-slot:avatar>
+                                <q-icon name="pin" color="text-white" />
+                            </template>
+                            Numer seryjny modułu: <strong>656</strong>
+                        </q-banner>
+                        <q-banner dense rounded class="bg-primary text-white">
+                            <template v-slot:avatar>
+                                <q-icon name="format_list_numbered" color="text-white" />
+                            </template>
+                            Ilość odczytów: <strong> {{ statistics.totalNumberReadout }}</strong>
+                        </q-banner>
+                        <q-banner dense rounded class="bg-primary text-white">
+                            <template v-slot:avatar>
+                                <q-icon name="water" color="text-white" />
+                            </template>
+                            Średni poziom wody: <strong>{{ statistics.avgWaterLevel }}</strong> mppt <br>
+                            Najwyższy poziom wody: <strong>{{ statistics.maxWaterLevel }}</strong> mppt<br>
+                            Najniższy poziom wody: <strong>{{ statistics.minWaterLevel }}</strong> mppt
+                        </q-banner>
+                        <q-banner dense rounded class="bg-primary text-white">
+                            <template v-slot:avatar>
+                                <q-icon name="thermostat" color="text-white" />
+                            </template>
+                            Średni temperatura powietrza: <strong>{{ statistics.avgTemperature }}</strong> °C <br>
+                            Najwyższa temperatura powietrza: <strong>{{ statistics.maxTemperature }}</strong> °C<br>
+                            Najniższa temperatura powietrza: <strong>{{ statistics.minTemperature }}</strong>
+                            °C
+                        </q-banner>
+                    </div>
+                </q-card>
+            </div>
         </div>
-        <div class="col-5 q-pa-md">
-            <q-card class="q-pa-lg" style="height: 480px" flat bordered>
-                <q-list>
-                    <q-item>
-                        <q-item-section avatar>
-                            <q-icon name="pin" class="text-primary" />
-                        </q-item-section>
-                        <q-item-section>Numer seryjny modułu:</q-item-section>
-                        <!-- <q-item-section side></q-item-section> -->
-                    </q-item>
-                    <q-item>
-                        <q-item-section avatar>
-                            <q-icon name="format_list_numbered" class="text-primary" />
-                        </q-item-section>
-                        <q-item-section>Ilość odczytów: {{ statistics.totalNumberReadout }}</q-item-section>
-                        <!-- <q-item-section side></q-item-section> -->
-                    </q-item>
-                    <q-item>
-
-                        <q-item-section avatar>
-                            <q-icon name="water" class="text-primary" />
-                        </q-item-section>
-                        <q-item-section>
-                            <q-item-label>Średni poziom wody: {{ statistics.avgWaterLevel }} mppt</q-item-label>
-                            <q-item-label caption>Najwyższy poziom wody: {{ statistics.maxWaterLevel }} mppt</q-item-label>
-                            <q-item-label caption>Najniższy poziom wody: {{ statistics.minWaterLevel }} mppt</q-item-label>
-                        </q-item-section>
-                    </q-item>
-                    <q-item>
-
-                        <q-item-section avatar>
-                            <q-icon name="thermostat" class="text-primary" />
-                        </q-item-section>
-                        <q-item-section>
-                            <q-item-label>Średni temperatura powietrza: {{ statistics.avgTemperature }} °C</q-item-label>
-                            <q-item-label caption>Najwyższa temperatura powietrza: {{ statistics.maxTemperature }} °C</q-item-label>
-                            <q-item-label caption>Najniższa temperatura powietrza: {{ statistics.minTemperature }} °C</q-item-label>
-                        </q-item-section>
-                    </q-item>
-                </q-list>
-            </q-card>
-        </div>
-    </div>
-</q-page>
+    </q-page>
 </template>
 
 <script>
